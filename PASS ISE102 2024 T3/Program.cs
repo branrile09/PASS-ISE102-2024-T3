da94af10ba8d881c42ed128f9fdbd64829ad3a66
branrile09 <93716194+branrile09@users.noreply.github.com>
2024-09-26 19:02:36 +0930

    Add project files.

---
+++ PASS ISE102 2024 T3/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace PASS_ISE102_2024_T3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+
+
+
+    }
+}
