cf39172a6ae3e10ab2ad9a9dbbb3165179629423
branrile09 <93716194+branrile09@users.noreply.github.com>
2024-09-26 19:39:23 +0930

    first week

--- PASS ISE102 2024 T3/Program.cs
+++ PASS ISE102 2024 T3/Program.cs
@@ -4,8 +4,32 @@
     {
         static void Main(string[] args)
         {
+
+            //Everyones hellos :)
             Console.WriteLine("Welcome everyone to PASS T3 for ISE102");
-        }
+
+            Console.WriteLine("Hello RB  here :) ");
+            Console.WriteLine("Hello Everyone Rob:) ");//yes
+        
+
+            Console.WriteLine("Hello everyone, JG"); //yes
+            Console.WriteLine("Hey Its Keer Shiman"); //yes            
+            Console.WriteLine("hi, this is Siddida"); //yes
+
+
+
+            //lets make some Variables
+            int rileyBransonsAge = 29;
+            float number = 3.3f;
+            double var;
+
+
+             Console.WriteLine("Whats Your Name");
+             // Capture users name 
+             string Name = Console.ReadLine();
+            // display username 
+            Console.WriteLine("Hi," + Name + "!");
+        } 
 
 
 
